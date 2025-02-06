af6fafb9d5b251e441a3c797370b1cb2c674986f
DavideSelvaggio <selvaggiodavide@hotmail.it>
2025-02-06 18:05:52 +0800

    Add thread admins

--- backend/threads/admin.py
+++ backend/threads/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Message, Thread, Run, Summary
 
-# Register your models here.
+
+@admin.register(Message)
+class MessageAdmin(ModelAdmin):
+    list_display = ["id", "thread", "created_at"]
+    search_fields = ["thread__contact_name"]
+    list_filter = ["created_at"]
+
+
+@admin.register(Thread)
+class ThreadAdmin(ModelAdmin):
+    list_display = ["id", "contact_name", "created_at"]
+    search_fields = ["contact_name"]
+    list_filter = ["created_at"]
+
+
+@admin.register(Run)
+class RunAdmin(ModelAdmin):
+    list_display = ["id", "thread", "created_at"]
+    search_fields = ["thread__contact_name"]
+    list_filter = ["created_at"]
+
+
+@admin.register(Summary)
+class SummaryAdmin(ModelAdmin):
+    list_display = ["id", "thread", "created_at"]
+    search_fields = ["thread__contact_name"]
+    list_filter = ["created_at"]
