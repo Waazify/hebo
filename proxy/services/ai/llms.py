4bee2fcd79fa62b1770b6d36e5567f06a07dffef
DavideSelvaggio <selvaggiodavide@hotmail.it>
2025-03-25 10:30:54 +0800

    support more bedrock models

--- proxy/services/ai/llms.py
+++ proxy/services/ai/llms.py
@@ -1,12 +1,22 @@
+import re
 from langchain_aws import ChatBedrockConverse
 
-PROVIDER_MAP = {
-    "arn:aws:bedrock:us-west-2:864981741310:inference-profile/us.anthropic.claude-3-5-sonnet-20240620-v1:0": ChatBedrockConverse,
-}
+# Define regex pattern for AWS Bedrock ARNs
+BEDROCK_ARN_PATTERN = re.compile(
+    r"arn:aws:bedrock:[a-z0-9-]+:\d+:inference-profile/[a-zA-Z0-9.-]+:\d+"
+)
+
+
+# Use a function to determine the provider based on the ARN
+def get_provider(model_name):
+    if BEDROCK_ARN_PATTERN.match(model_name):
+        return ChatBedrockConverse
+    raise ValueError(f"Unsupported model: {model_name}")
 
 
 def init_llm(client, model_name, temperature=1, max_tokens=512):
-    return PROVIDER_MAP[model_name](
+    provider = get_provider(model_name)
+    return provider(
         client=client,
         model=model_name,
         temperature=temperature,
