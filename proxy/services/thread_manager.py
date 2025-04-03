32fb1c9a821d351fafb56d994756e7ade5587669
DavideSelvaggio <selvaggiodavide@hotmail.it>
2025-04-03 18:33:03 +0800

    fix agent message between 2 tool messages in error

--- proxy/services/thread_manager.py
+++ proxy/services/thread_manager.py
@@ -650,16 +650,28 @@ class ThreadManager:
 
         # The first message is always sent
         if len([m for m in conversation_messages if isinstance(m, AIMessage)]) == 0:
-            return True
+            if (
+                len(
+                    [
+                        reply
+                        for reply in replies
+                        if reply.message_type == MessageType.TOOL_ANSWER
+                    ]
+                )
+                == 0
+            ):
+                return True
 
         if not hide_tool_messages:
             return True
 
         # We send message between two tool calls, unless the tool call is an error
         if len(replies) > 1 and replies[-2].message_type == MessageType.TOOL_ANSWER:
-            if "error" in [
-                c.text.lower() for c in replies[-2].content if c.text is not None
-            ] and any(
+            if any(
+                "error" in c.text.lower()
+                for c in replies[-2].content
+                if c.text is not None
+            ) and any(
                 tool_call.name == replies[-2].tool_call_name
                 for tool_call in replies[-1].content
             ):
